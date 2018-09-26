24aa4f51518f99d480d6e54bd6caaff845beb2b1
Nikita Jajodia <nikitajajodia@Nikita.local>
2018-09-26 14:26:52 +0530

    install redux-form and immutable dependencies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux-immutable';
+
+import {
+  reducer as form,
+} from 'redux-form/immutable';
 
 import authObject from './AuthReducer';
 
 export default combineReducers({
-	authObject
+	authObject,
+	form,
 });
