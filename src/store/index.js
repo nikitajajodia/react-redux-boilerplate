baa235cb43d2b9ccbbec555d73c2c71bf0f6b5e8
Nikita Jajodia <nikitajajodia@Nikita.local>
2018-11-26 22:05:00 +0530

    include redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,19 @@ import {
   compose,
 } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 export default function configureStore(initialState) {
+  const middlewares = [];
+
+  if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+  }
+
   const sagaMiddleware = createSagaMiddleware();
+  middlewares.push(sagaMiddleware);
 
   // Creates the Redux store using our reducer and the logger and saga middlewares
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -16,7 +24,7 @@ export default function configureStore(initialState) {
     rootReducer,
     initialState,
     composeEnhancers(
-      applyMiddleware(sagaMiddleware),
+      applyMiddleware(...middlewares),
     ),
   );
   // We run the root saga automatically
